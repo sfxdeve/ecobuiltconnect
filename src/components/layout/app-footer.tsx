9edd0aa645030bdcf4edfc6c58120c3f617ea4a2
Shayan Fareed <sfx.deve@gmail.com>
2025-12-21 17:36:13 +0500

    feat: enhance AppHeader and AppFooter components with improved layout and navigation highlighting
    - Updated AppFooter to include a copyright notice and improved styling.
    - Refactored AppHeader to highlight the active navigation link based on the current route.
    - Added ShoppingCartIcon button to the header for better user interaction.

--- src/components/layout/app-footer.tsx
+++ src/components/layout/app-footer.tsx
@@ -1,3 +1,13 @@
 export function AppFooter() {
-	return <footer></footer>;
+	return (
+		<footer className={"bg-muted"}>
+			<div className={"container mx-auto py-12"}></div>
+			<div className={"py-4 bg-primary"}>
+				<p className={"text-center text-white text-sm"}>
+					Copyright © 2025 Ecobuilt. All rights reserved. Privacy Policy | Terms
+					of Service
+				</p>
+			</div>
+		</footer>
+	);
 }
--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -4,86 +4,94 @@ import {
 	SignInButton,
 	UserButton,
 } from "@clerk/clerk-react";
-import { Link } from "@tanstack/react-router";
-import { buttonVariants } from "@/components/ui/button";
+import { Link, useLocation } from "@tanstack/react-router";
+import { ShoppingCartIcon } from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export function AppHeader() {
+	const location = useLocation();
+
 	return (
 		<header
 			className={"container mx-auto flex items-center justify-between py-4"}
 		>
 			{/* Logo */}
-			<div>
-				<h1>EcobuiltConnect</h1>
-			</div>
+			<h1>EcobuiltConnect</h1>
+
 			{/* Links */}
-			<div>
-				<ul className={"flex gap-4 items-center"}>
-					<li>
-						<Link
-							className={cn(
-								"",
-								buttonVariants({ variant: "ghost", size: "lg" }),
-							)}
-							to="/"
-						>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={cn(
-								"",
-								buttonVariants({ variant: "ghost", size: "lg" }),
-							)}
-							to="/products"
-						>
-							Marketplace
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={cn(
-								"",
-								buttonVariants({ variant: "ghost", size: "lg" }),
-							)}
-							to="/vendors"
-						>
-							Vendors
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={cn(
-								"",
-								buttonVariants({ variant: "ghost", size: "lg" }),
-							)}
-							to="/community"
-						>
-							Community
-						</Link>
-					</li>
-					<li>
-						<Link
-							className={cn(
-								"",
-								buttonVariants({ variant: "ghost", size: "lg" }),
-							)}
-							to="/contact"
-						>
-							Contact
-						</Link>
-					</li>
-				</ul>
-			</div>
+			<ul className={"flex gap-4 items-center"}>
+				<li>
+					<Link
+						to="/"
+						className={cn(
+							buttonVariants({ variant: "ghost", size: "lg" }),
+							location.pathname === "/" && "bg-muted",
+						)}
+					>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/products"
+						className={cn(
+							buttonVariants({ variant: "ghost", size: "lg" }),
+							location.pathname === "/products" && "bg-muted",
+						)}
+					>
+						Marketplace
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/vendors"
+						className={cn(
+							buttonVariants({ variant: "ghost", size: "lg" }),
+							location.pathname === "/vendors" && "bg-muted",
+						)}
+					>
+						Vendors
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/community"
+						className={cn(
+							buttonVariants({ variant: "ghost", size: "lg" }),
+							location.pathname === "/community" && "bg-muted",
+						)}
+					>
+						Community
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/contact"
+						className={cn(
+							buttonVariants({ variant: "ghost", size: "lg" }),
+							location.pathname === "/contact" && "bg-muted",
+						)}
+					>
+						Contact
+					</Link>
+				</li>
+			</ul>
+
 			{/* Actions */}
-			<div>
+			<div className={"flex items-center gap-4"}>
+				<Button variant="outline" size="icon">
+					<ShoppingCartIcon className="size-4.5" />
+				</Button>
 				<SignedIn>
 					<UserButton />
 				</SignedIn>
 				<SignedOut>
-					<SignInButton />
+					<div
+						className={cn(buttonVariants({ variant: "default", size: "lg" }))}
+					>
+						<SignInButton />
+					</div>
 				</SignedOut>
 			</div>
 		</header>
