8e076dbff74867f83db2cab6e09b19c81ec75964
Shayan Fareed <sfx.deve@gmail.com>
2025-12-23 22:35:04 +0500

    hide limit select on mobile fix

--- src/routes/(public)/products/index.tsx
+++ src/routes/(public)/products/index.tsx
@@ -246,7 +246,7 @@ function ProductsPagePagination() {
 						})
 					}
 				>
-					<SelectTrigger className="w-20 hidden md:block" id={limitSelectId}>
+					<SelectTrigger className="w-20 hidden md:flex" id={limitSelectId}>
 						<SelectValue />
 					</SelectTrigger>
 					<SelectContent align="start">
