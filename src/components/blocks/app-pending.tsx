4f72a077c4a773c5d80bee48a5fe9981573b01df
Shayan Fareed <sfx.deve@gmail.com>
2025-12-28 12:37:59 +0500

    refactor: making spinner bigger

--- src/components/blocks/app-pending.tsx
+++ src/components/blocks/app-pending.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/ui/spinner";
 export function AppPending() {
 	return (
 		<section className="container mx-auto flex h-[calc(100vh-16rem)] flex-col items-center justify-center gap-4 px-4 py-12">
-			<Spinner />
+			<Spinner className="size-6" />
 			<p className="text-center font-semibold text-muted-foreground">
 				Loading...
 			</p>
