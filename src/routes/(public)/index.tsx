170fb5aaf6530421f5870c109f61fccc58ea2706
Shayan Fareed <sfx.deve@gmail.com>
2026-01-01 18:45:50 +0500

    feat: Add step-by-step guide and customer testimonials sections to homepage for enhanced user engagement

--- src/routes/(public)/index.tsx
+++ src/routes/(public)/index.tsx
@@ -1,6 +1,24 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+	ArrowDownIcon,
+	ArrowRightIcon,
+	CreditCardIcon,
+	type LucideIcon,
+	ShoppingCartIcon,
+	TruckIcon,
+	User2Icon,
+} from "lucide-react";
 import { AppPending } from "@/components/blocks/app-pending";
 import { buttonVariants } from "@/components/ui/button";
+import {
+	Item,
+	ItemContent,
+	ItemDescription,
+	ItemGroup,
+	ItemHeader,
+	ItemMedia,
+	ItemTitle,
+} from "@/components/ui/item";
 import { cn } from "@/utils";
 
 export const Route = createFileRoute("/(public)/")({
@@ -21,6 +39,61 @@ export const Route = createFileRoute("/(public)/")({
 });
 
 function HomePage() {
+	const steps: {
+		icon: LucideIcon;
+		title: string;
+		description: string;
+	}[] = [
+		{
+			icon: ShoppingCartIcon,
+			title: "Step 1",
+			description:
+				"Browse our marketplace and select the construction materials you need.",
+		},
+		{
+			icon: CreditCardIcon,
+			title: "Step 2",
+			description: "Add items to your cart and proceed to checkout.",
+		},
+		{
+			icon: User2Icon,
+			title: "Step 3",
+			description:
+				"Sign in to your account and verify your credentials with OTP.",
+		},
+		{
+			icon: TruckIcon,
+			title: "Step 4",
+			description:
+				"Choose your delivery method, process payment, and you're done!",
+		},
+	];
+
+	const testimonials: {
+		image: string;
+		name: string;
+		message: string;
+	}[] = [
+		{
+			image: "/face-01.webp",
+			name: "Jack Reul",
+			message:
+				"Finally, a platform that makes finding quality recycled construction materials easy and reliable.",
+		},
+		{
+			image: "/face-02.webp",
+			name: "Jessy Iris",
+			message:
+				"EcobuiltConnect helped us cut material costs by 20% while staying eco-friendly — a game changer!",
+		},
+		{
+			image: "/face-03.webp",
+			name: "Mark Keller",
+			message:
+				"EcobuiltConnect bridges the gap between sustainability and affordability!",
+		},
+	];
+
 	return (
 		<>
 			<section>
@@ -99,7 +172,7 @@ function HomePage() {
 				</div>
 			</section>
 			<section>
-				<div className="container mx-auto py-24 px-4">
+				<div className="container mx-auto py-24 px-4 space-y-12">
 					<div className="space-y-12">
 						<div className="max-w-3xl mx-auto space-y-6 text-center">
 							<h2 className="text-3xl md:text-6xl font-medium">
@@ -113,6 +186,68 @@ function HomePage() {
 								the construction industry!
 							</p>
 						</div>
+					</div>
+					<div>
+						<ItemGroup className="max-w-5xl mx-auto md:flex-row items-center justify-center">
+							{steps.map((step) => (
+								<>
+									<Item key={step.title} variant="outline" className="w-48">
+										<ItemHeader className="py-2 justify-center">
+											<div className="p-5 rounded-full flex flex-col justify-center items-center bg-primary text-white">
+												<step.icon className="size-8" />
+											</div>
+										</ItemHeader>
+										<ItemContent>
+											<ItemTitle className="w-full justify-center">
+												{step.title}
+											</ItemTitle>
+											<ItemDescription className="line-clamp-none text-center">
+												{step.description}
+											</ItemDescription>
+										</ItemContent>
+									</Item>
+									{step !== steps[steps.length - 1] && (
+										<>
+											<ArrowRightIcon className="size-8 hidden md:block" />
+											<ArrowDownIcon className="size-8 md:hidden" />
+										</>
+									)}
+								</>
+							))}
+						</ItemGroup>
+					</div>
+				</div>
+			</section>
+			<section>
+				<div className="container mx-auto py-24 px-4 space-y-12">
+					<div className="space-y-12">
+						<div className="max-w-3xl mx-auto space-y-6 text-center">
+							<h2 className="text-3xl md:text-6xl font-medium">
+								<span>Hear from Our</span>{" "}
+								<span className="text-primary">Satisfied Customers!</span>
+							</h2>
+							<p className="text-lg">
+								See what builders, architects, and sustainability champions are
+								saying about EcobuiltConnect!
+							</p>
+						</div>
+					</div>
+					<div>
+						<ItemGroup className="max-w-5xl mx-auto md:flex-row items-center justify-center">
+							{testimonials.map((testimonial) => (
+								<Item key={testimonial.name} variant="outline">
+									<ItemMedia variant="image">
+										<img src={testimonial.image} alt={testimonial.name} />
+									</ItemMedia>
+									<ItemContent>
+										<ItemTitle>{testimonial.name}</ItemTitle>
+										<ItemDescription className="line-clamp-none">
+											{testimonial.message}
+										</ItemDescription>
+									</ItemContent>
+								</Item>
+							))}
+						</ItemGroup>
 					</div>
 				</div>
 			</section>
